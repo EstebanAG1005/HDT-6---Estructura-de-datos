0cbb20bd480f5c5d71224aaadc7b3b2b0571bc5e
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-21 16:05:39 -0600

    factory

--- Factory.java
+++ Factory.java
@@ -1,21 +1,21 @@
 public class Factory <E>{
     
     
-    public Hashmap getHashmaps(int resp1) {
+    public Hashmap getHashmaps(int op) {
         // seleccion de la implementacion a utilizar:
-            if (resp1 == 1){
+            if (op == 1){
                 return new Hashmap(); //regresa ArrayList
             }
             return null;
        }
-    public LinkedHashMaps getLinkedHashMaps(int resp1){
-        if(resp1 == 2){
+    public LinkedHashMaps getLinkedHashMaps(int op){
+        if(op == 2){
             return new LinkedHashMaps();
         }
         return null;
     }
-    public TreeMaps getTreeMaps(int resp1){
-        if(resp1 == 3){
+    public TreeMaps getTreeMaps(int op){
+        if(op == 3){
             return new TreeMaps();
         }
         return null;
