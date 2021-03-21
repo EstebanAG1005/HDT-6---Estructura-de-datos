fe6fb791f795807f33cfd07d53874f4d1cc800fa
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2021-03-21 10:46:59 -0600

    main

--- main.java
+++ main.java
@@ -1,3 +1,48 @@
-public class main{
-    
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+
+        int op = 0;
+        boolean salir = true;
+        Scanner sc = new Scanner(System.in);
+
+        
+            while (salir) {
+
+                try {
+                System.out.println("Seleccione de que forma desea guardar sus datos");
+                System.out.println("1. HashMap");
+                System.out.println("2. TreeMap");
+                System.out.println("3. LinkedHashMap");                
+                System.out.println("4. Salir");
+                System.out.print("Seleccione la opcion a realizar: ");
+                op = sc.nextInt();
+
+                if (op == 1){
+
+                }
+                else if(op == 2){
+
+                }
+                else if(op == 3){
+
+                }
+                else if(op == 4){
+
+                }
+                else{
+                    System.out.println("Solo puede ingresar numeros del 1 al 4\n");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Solo se pueden ingresar numeros\n");
+                sc.next();
+            }
+        }
+    }
 }
+
+             
+                        
+
+                   
