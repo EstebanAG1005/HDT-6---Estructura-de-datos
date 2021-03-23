cdc4b95cf0cdf97d0dfea68bbe2bb11e1c754e60
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-03-23 13:17:33 -0600

    abriendo el archivo en ejecución

--- ejecucion.java
+++ ejecucion.java
@@ -2,6 +2,7 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList; //ArrayList para las cartas.
 
 public class ejecucion {
    public void abrirarchivo(){
