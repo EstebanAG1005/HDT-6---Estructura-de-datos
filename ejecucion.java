025926784908e1066ed95a41c0ef211797d877b3
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-03-23 10:12:57 -0600

    Abriendo el archivo

---
+++ ejecucion.java
@@ -0,0 +1,13 @@
+//Imports para trabajar el archivo con las cartas. 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ejecucion {
+   public void abrirarchivo(){
+    String filePath = "cards_desc.txt";
+    String line;
+    BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+   }  
+}
